0401440b9da083816a53fad694c46eaaa126e7d1
khaizon <kaifeng_toh@mymail.sutd.edu.sg>
2021-03-10 14:53:14 +0800

    test

--- PA1/shellPrograms/shellDaemonize_code.c
+++ PA1/shellPrograms/shellDaemonize_code.c
@@ -13,7 +13,7 @@
 #include "shellPrograms.h"
 
 //TODO: change to appropriate path
-char *path = "/Users/natalie_agus/Dropbox/50.005 Computer System Engineering/2020/PA1 Makeshell Daemon/PA1/logfile_test.txt";
+char *path = "/home/khaizon/documents/ProgrammingAssignment1/PA1/logfile_test.txt";
 
 /*This function summons a daemon process out of the current process*/
 static int create_daemon()
@@ -31,6 +31,42 @@ static int create_daemon()
     // 7. Change working directory to root
     // 8. Close all open file descriptors using sysconf(_SC_OPEN_MAX) and redirect fd 0,1,2 to /dev/null
     // 9. Return to main
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        
+        setsid(); // child tries setsid
+        
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGHUP, SIG_IGN);
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            umask(0);
+            chdir("/");
+            /* Close all open file descriptors */
+            int x;
+            for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--)
+            {
+                close(x);
+            }
+            /* Attach file descriptors 0, 1, and 2 to /dev/null. */
+            int fd0 = open("/dev/null", O_RDWR);
+            int fd1 = dup(0);
+            int fd2 = dup(0);
+
+            return 1;
+        }
+        else if (pid < 0){perror("smth wong");}
+        else
+            exit(1); //parent process immediately terminates
+    }
+    else if (pid < 0){perror("smth wong");}
+    else
+    {
+        exit(1); // instruction '2' for parent
+    }
 
     return 1;
 }
@@ -53,37 +89,31 @@ static int daemon_work()
 
     while (1)
     {
-
         //use appropriate location if you are using MacOS or Linux
         //TODO: Change to appropriate path
         fptr = fopen(path, "a");
-
         if (fptr == NULL)
         {
             return EXIT_FAILURE;
         }
-
         fprintf(fptr, "PID %d Daemon writing line %d to the file.  \n", getpid(), num);
         num++;
-
         fclose(fptr);
-
         sleep(10);
-
         if (num == 10)
             break;
     }
 
     return EXIT_SUCCESS;
 }
-int main(int argc, char **args)
+int main(int argc, char** args)
 {
-    create_daemon();
-
-    /* Open the log file */
-    openlog("customdaemon", LOG_PID, LOG_DAEMON);
-    syslog(LOG_NOTICE, "Daemon started.");
-    closelog();
-
-    return daemon_work();
+   create_daemon();
+  
+   /* Open the log file */
+   openlog ("customdaemon", LOG_PID, LOG_DAEMON);
+   syslog (LOG_NOTICE, "Daemon started.");
+   closelog();
+ 
+   return daemon_work();
 }
